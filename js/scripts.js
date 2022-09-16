66c73fce518408eb49bc5303d715299be9d1e3f8
anriadamia <anriadamia2000@gmail.com>
2022-09-16 09:31:40 +0400

    დავალება X

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function antebachakroba(){
+    let natura=document.getElementById("light");
+    if(natura.src.match("img/off.png"))
+    {natura.src="img/on.png"}
+    else{natura.src="img/off.png"}
+}
